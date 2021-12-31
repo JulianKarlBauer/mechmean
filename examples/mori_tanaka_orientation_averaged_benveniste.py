bdbd3c3173a9a5b3ae6530a3678dd1ff003b0cf5
Julian Karl Bauer <juliankarlbauer@gmx.de>
2021-12-31 13:16:09 +0100

    Restructure examples

---
+++ examples/mori_tanaka_orientation_averaged_benveniste.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import mechmean
+import mechkit
+from input import inp
+
+# Define isotropic constituents
+inclusion = mechkit.material.Isotropic(E=inp["E_f"], nu=inp["nu_f"])
+matrix = mechkit.material.Isotropic(E=inp["E_m"], nu=inp["nu_m"])
+
+# Define orientation averager and polairzation
+averager = mechmean.orientation_averager.AdvaniTucker(N4=inp["N4"])
+P_func = mechmean.hill_polarization.Factory().needle
+
+# Homogenize
+input_dict = {
+    "phases": {
+        "inclusion": {
+            "material": inclusion,
+            "volume_fraction": inp["c_f"],
+            "hill_polarization": P_func(matrix=matrix),
+        },
+        "matrix": {"material": matrix},
+    },
+    "k": inp["k"],
+    "averaging_func": averager.average,
+}
+mori = mechmean.approximation.MoriTanakaOrientationAveragedBenveniste(**input_dict)
+C_eff = mori.calc_C_eff()
+
+print("Effective stiffness Mori-Tanaka orientation averaged Benveniste", C_eff)
---
+++ examples/two_step_hashin_shtrikman_interpolated.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import mechmean
+import mechkit
+from input import inp
+
+# Define isotropic constituents
+inclusion = mechkit.material.Isotropic(E=inp["E_f"], nu=inp["nu_f"])
+matrix = mechkit.material.Isotropic(E=inp["E_m"], nu=inp["nu_m"])
+
+# Define orientation averager and polairzation
+averager = mechmean.orientation_averager.AdvaniTucker(N4=inp["N4"])
+P_func = mechmean.hill_polarization.Factory().needle
+
+# Homogenize
+input_dict = {
+    "phases": {
+        "inclusion": {
+            "material": inclusion,
+            "volume_fraction": inp["c_f"],
+        },
+        "matrix": {
+            "material": matrix,
+            "volume_fraction": 1.0 - inp["c_f"],
+        },
+    },
+    "k": inp["k"],
+    "averaging_func": averager.average,
+}
+hashin = mechmean.approximation.Kehrer2019(**input_dict)
+C_eff = hashin.calc_C_eff()
+
+print("Effective stiffness Kehrer2019", C_eff)
