35ea3d821a5fd29cabf3ae08866a87f1a5ed77a4
Julian Karl Bauer <juliankarlbauer@gmx.de>
2021-12-31 13:54:33 +0100

    Add third example

---
+++ examples/two_step_hashin_shtrikman_interpolated_twice.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import mechmean
+import mechkit
+from mechmean.example_input import inp
+
+# Define isotropic constituents
+inclusion = mechkit.material.Isotropic(E=inp["E_f"], nu=inp["nu_f"])
+matrix = mechkit.material.Isotropic(E=inp["E_m"], nu=inp["nu_m"])
+
+# Define orientation averager and polairzation
+averager = mechmean.orientation_averager.AdvaniTucker(N4=inp["N4"])
+P_func = mechmean.hill_polarization.Factory().needle
+
+# Homogenize
+input_dict = {
+    "phases": {
+        "inclusion": {
+            "material": inclusion,
+            "volume_fraction": inp["c_f"],
+        },
+        "matrix": {
+            "material": matrix,
+            "volume_fraction": 1.0 - inp["c_f"],
+        },
+    },
+    "k1": 1.0 / 2.0,
+    "k2": 1.0 / 2.0,
+    "averaging_func": averager.average,
+}
+hashin = mechmean.approximation.HSW2StepInterpolatedReferenceMaterial(**input_dict)
+C_eff = hashin.calc_C_eff()
+
+print("Effective stiffness Hashin Shtrikman two step with two interpolations", C_eff)
